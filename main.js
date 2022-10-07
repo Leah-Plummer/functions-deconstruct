7695c1775963cf57cae42fde51f2db19f0985841
Leah Plummer <leahplumm@gmail.com>
2022-10-07 10:10:21 -0500

    basic practice with functions

---
+++ main.js
@@ -0,0 +1,47 @@
+    /* Functions will represent individual 
+        tasks involved in making coffee.*/
+
+        /* Tasks
+        1. pour beans in grinder
+        2. grind beans
+        3. put coffee filter in coffee maker
+        4. pour grounds into filter
+        5. insert grounds into coffee maker
+        6. pour water into coffee maker
+        7. turn on coffee maker to brew coffee */
+
+const pourBeans = () => {
+    console.log(`pour coffee beans into the coffee grinder up to fill line.`)
+}  
+
+const grindBeans = () => {
+    console.log(`hold button on grinder until desired fineness is achieved`)
+}   
+
+const coffeeFilter = () => {
+    console.log(`place a coffee filter into the receptacle`)
+}
+
+const putGrounds = () => {
+    console.log(`pour the grounds into the filter`)
+}
+
+const insertReceptacle = () => {
+    console.log(`insert the receptacle for the filter and grounds into the coffee maker`)
+}
+
+const addWater = () => {
+    console.log(`add water into the coffee maker up to fill line`)
+}
+
+const brewCoffee = () => {
+    console.log(`press start button to brew your coffee`)
+}
+
+pourBeans()
+grindBeans()
+coffeeFilter()
+putGrounds()
+insertReceptacle()
+addWater()
+brewCoffee()
